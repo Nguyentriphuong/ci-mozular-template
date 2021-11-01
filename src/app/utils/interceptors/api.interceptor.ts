a0336b221f40d6a31499f06694ee5ac292be71c5
Nguyentriphuong <Nguyentriphuong2107@gmail.com>
2021-11-01 10:28:33 +0700

    remove auth

--- src/app/utils/interceptors/api.interceptor.ts
+++ src/app/utils/interceptors/api.interceptor.ts
@@ -13,13 +13,17 @@ export class APIInterceptor implements HttpInterceptor {
         const token = localStorage.getItem('access_token');
         let request;
         if (!localStorage.getItem('watting') || req.method !== 'POST' ) {
-          request = req.clone({
-            url: `${req.url}`,
-            setHeaders: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-          });
+          if (req.url.includes('json')) {
+            request = req.clone();
+          } else {
+            request = req.clone({
+              url: `${req.url}`,
+              setHeaders: {
+                  Authorization: `Bearer ${token}`,
+                  'Content-Type': 'application/json',
+              },
+            });
+          }
         }
         localStorage.setItem('watting', 'yes');
         setTimeout(() => {
